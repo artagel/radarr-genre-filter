81d712a67a50b9bea870b478561ec97e174ca479
artagel <dan@syssec.biz>
2020-05-05 19:44:55 +0800

    Add security note and environment keys to settings.py

--- settings.py
+++ settings.py
@@ -1,16 +1,19 @@
 import sys
 import os
 
+# Security note: Never store api keys in files.
+# We use environment keys for that.
+
 scriptdir = os.path.dirname(os.path.realpath(sys.argv[0]))
 # INFO, DEBUG, WARNING, ERROR, CRITICAL
 log_level = 'INFO'
-# Radarr's API Key for TMDB
+# Radarr's API Key for TMDB.  Stored because it's publicly available.
 radarr_tmdb_api_key = '1a7373301961d03f97f853a876dd1212'
 tmdb_url = 'https://api.themoviedb.org/3'
-# Your Radarr API url
-radarr_url = 'http://your_radar_url:7878/api'
+# Your Radarr API url. Example: http://radarr.my.server:7878/api
+radarr_url = os.environ['RADARR_API_URL']
 # Your Radarr API key
-radarr_api_key = ''
+radarr_api_key = os.environ['RADARR_API_KEY']
 # Turn this False if you really need to.
 verify_ssl_certs = True
 # The file where we save the state of the genres, 
